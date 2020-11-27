5a4e65baba989cbc49f7252b14b0cb0d2c1387cd
VladMurarescu <murarescuvladeusebiu@gmail.com>
2020-11-27 12:06:22 +0200

    strapi on heroku

--- src/Url.js
+++ src/Url.js
@@ -1,4 +1,11 @@
-export const servicesUrl = "http://localhost:1337/services";
-export const productsUrl = "http://localhost:1337/products";
-export const infosUrl = "http://localhost:1337/infos";
-export const dataUrl = "http://localhost:1337/data";
+export const servicesUrl =
+  "https://croitoria-cuti-strapi.herokuapp.com/services";
+export const productsUrl =
+  "https://croitoria-cuti-strapi.herokuapp.com/products";
+export const infosUrl = "https://croitoria-cuti-strapi.herokuapp.com/infos";
+export const dataUrl = "https://croitoria-cuti-strapi.herokuapp.com/data";
+
+// export const servicesUrl = "http://localhost:1337/services";
+// export const productsUrl = "http://localhost:1337/products";
+// export const infosUrl = "http://localhost:1337/infos";
+// export const dataUrl = "http://localhost:1337/data";
